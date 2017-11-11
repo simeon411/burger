ec5985ed8201309cd22edaf94c0569dc4302bc08
simeon411 <simeon411@users.noreply.github.com>
2017-11-11 07:42:49 -0800

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheddar');
+INSERT INTO burgers (burger_name) VALUES ('Basic Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
